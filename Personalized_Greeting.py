d4c1f1dbf882712f18e5e2ec7d147d14a00baa50
dani6566 <danielhailay72@gmail.com>
2025-08-11 19:30:01 +0300

    Add personalized greeting

---
+++ Personalized_Greeting.py
@@ -0,0 +1,5 @@
+user_name = input("Enter your user name: ")  #Get the user name
+favorite_color = input("Enter your favorite color: ")  #Get the user favorite color
+
+#display the favorite color
+print(f"Hello, {user_name}! Your favorite color, {favorite_color}, is awesome!")
